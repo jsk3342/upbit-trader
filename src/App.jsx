7586a0512dbb168b22711790d63923ceb43dd22c
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2023-01-05 08:34:19 +0900

    chore: init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CoinBoard from './pages/CoinBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <CoinBoard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/CoinBoard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CoinBoard() {
+    return (
+        <div>
+            테스트
+        </div>
+    );
+}
+
+export default CoinBoard;
